33ab6a70c05f88524129b0ed5cb67b7db0357dbe
yasufumi <yasufumi.taniguchi@gmail.com>
2018-01-31 17:45:49 +0900

    update main.py

--- main.py
+++ main.py
@@ -1,95 +1,116 @@
 from argparse import ArgumentParser
+from operator import le
+import logging
 
-import torch
 import torch.optim as optim
+from torch.nn import DataParallel
+from ignite.trainer import Trainer, TrainingEvents
+from ignite.handlers import Validate
 from torchtext import data
+from torchtext.datasets.translation import WMT14
 
 from model import Seq2Seq
 from data import SmallEnJa
+from utils import log_training_average_nll, get_log_validation_ppl, \
+    TeacherForceUpdater, TeacherForceInference, ComputeBleu, BestModelSnapshot
 
 
-parser = ArgumentParser()
-parser.add_argument('--epoch', type=int, default=100)
-parser.add_argument('--batch', type=int, default=64)
-parser.add_argument('--gpu', type=int, default=-1)
-parser.add_argument('--embed', type=int, default=512)
-parser.add_argument('--encoder-hidden', type=int, default=512)
-parser.add_argument('--decoder-hidden', type=int, default=512)
-parser.add_argument('--attention-type', type=str, default='general',
-                    choices=('general', 'mlp', 'dot'))
-parser.add_argument('--learning-rate', type=float, default=1e-3)
-parser.add_argument('--dropout-ratio', type=float, default=0.5)
-parser.add_argument('--weight-decay', type=float, default=1e-6)
-parser.add_argument('--gradient-clipping', type=int, default=5)
-parser.add_argument('--evaluation-step', type=int, default=3)
-parser.add_argument('--max-length', type=int, default=32)
-args = parser.parse_args()
+def main(args, logger):
+    SRC = data.Field(init_token='<s>', eos_token='</s>', include_lengths=True)
+    TRG = data.Field(init_token='<s>', eos_token='</s>')
+
+    if args.dataset == 'enja':
+        train, val, test = SmallEnJa.splits(
+            exts=('.en', '.ja'), fields=(SRC, TRG))
+    elif args.dataset == 'wmt14':
+        train, val, test = WMT14.splits(
+            exts=('.en', '.de'), fields=(SRC, TRG),
+            filter_pred=lambda ex: len(ex.src) <= 50 and len(ex.trg) <= 50)
+
+    SRC.build_vocab(train.src, max_size=args.src_vocab)
+    TRG.build_vocab(train.trg, max_size=args.trg_vocab)
+
+    stoi = TRG.vocab.stoi
+
+    train_iter, val_iter = data.BucketIterator.splits(
+        (train, val), batch_sizes=(args.batch, args.batch * 2), repeat=False,
+        sort_within_batch=True, device=args.gpu[0] if args.gpu else None,
+        sort_key=SmallEnJa.sort_key)
+    test_iter = data.Iterator(
+        test, batch_size=1, repeat=False, sort=False, train=False,
+        device=args.gpu[0] if args.gpu else None)
+
+    model = Seq2Seq(
+        len(SRC.vocab), args.embed, args.encoder_hidden,
+        len(TRG.vocab), args.embed, args.decoder_hidden, args.encoder_layers,
+        not args.encoder_unidirectional, args.decoder_layers,
+        SRC.vocab.stoi[SRC.pad_token], stoi[TRG.pad_token],
+        args.dropout_ratio, args.attention_type)
+    model.prepare_translation(stoi[TRG.init_token], stoi[TRG.eos_token],
+                              TRG.vocab.itos, args.max_length)
+
+    compute_loss = model.compute_loss
+    translate = model.translate
+    if len(args.gpu) >= 2:
+        compute_loss = DataParallel(compute_loss, device_ids=args.gpu, dim=1)
+    if args.gpu:
+        model.cuda(args.gpu[0])
+
+    optimizer = optim.Adam(model.parameters(), lr=args.learning_rate,
+                           weight_decay=args.weight_decay)
+
+    trainer = Trainer(
+        TeacherForceUpdater(model, optimizer, compute_loss, args.gradient_clipping),
+        TeacherForceInference(model, compute_loss))
+    trainer.add_event_handler(TrainingEvents.EPOCH_COMPLETED,
+                              Validate(val_iter, epoch_interval=1))
+    trainer.add_event_handler(TrainingEvents.TRAINING_EPOCH_COMPLETED,
+                              log_training_average_nll, logger=logger)
+    trainer.add_event_handler(TrainingEvents.VALIDATION_COMPLETED,
+                              get_log_validation_ppl(val.trg), logger=logger)
+    trainer.add_event_handler(TrainingEvents.VALIDATION_COMPLETED,
+                              BestModelSnapshot(model, 'ppl', 1e10, le),
+                              args.best_file, logger)
+    trainer.add_event_handler(TrainingEvents.TRAINING_COMPLETED,
+                              ComputeBleu(model, test.trg, translate),
+                              test_iter, args.best_file, logger)
+    trainer.run(train_iter, max_epochs=args.epoch)
 
 
-EN = data.Field(eos_token='</s>')
-JA = data.Field(init_token='<s>', eos_token='</s>')
+if __name__ == '__main__':
+    parser = ArgumentParser()
+    parser.add_argument('--epoch', type=int, default=100)
+    parser.add_argument('--batch', type=int, default=64)
+    parser.add_argument('--gpu', type=int, default=[], nargs='+')
+    parser.add_argument('--embed', type=int, default=512)
+    parser.add_argument('--src-vocab', type=int, default=4000)
+    parser.add_argument('--encoder-hidden', type=int, default=512)
+    parser.add_argument('--encoder-layers', type=int, default=1)
+    parser.add_argument('--encoder-unidirectional',
+                        action='store_true', default=False)
+    parser.add_argument('--trg-vocab', type=int, default=5000)
+    parser.add_argument('--decoder-hidden', type=int, default=512)
+    parser.add_argument('--decoder-layers', type=int, default=1)
+    parser.add_argument(
+        '--attention-type', type=str, default='concat',
+        choices=('general', 'concat', 'dot'))
+    parser.add_argument('--learning-rate', type=float, default=1e-3)
+    parser.add_argument('--dropout-ratio', type=float, default=0.5)
+    parser.add_argument('--weight-decay', type=float, default=1e-6)
+    parser.add_argument('--gradient-clipping', type=int, default=5)
+    parser.add_argument('--max-length', type=int, default=32)
+    parser.add_argument('--log-file', type=str, default=None)
+    parser.add_argument('--best-file', type=str, default='best.model')
+    parser.add_argument('--dataset', type=str, default='enja',
+                        choices=('enja', 'wmt14'))
+    args = parser.parse_args()
 
-train, val, test = SmallEnJa.splits(exts=('.en', '.ja'), fields=(EN, JA))
+    if args.log_file is not None:
+        logging.basicConfig(filename=args.log_file, level=logging.INFO)
+        logger = logging.getLogger()
+        logger.addHandler(logging.StreamHandler())
+        logger = logger.info
+    else:
+        logger = print
 
-
-EN.build_vocab(train.src, max_size=4000)
-JA.build_vocab(train.trg, max_size=5000)
-
-
-train_iter, val_iter, test_iter = data.BucketIterator.splits(
-    (train, val, test), batch_sizes=(args.batch, 512, 512), device=args.gpu)
-
-stoi = JA.vocab.stoi
-model = Seq2Seq(
-    len(EN.vocab), args.embed, args.encoder_hidden,
-    len(JA.vocab), args.embed, args.decoder_hidden,
-    EN.vocab.stoi[EN.pad_token], stoi[JA.pad_token], args.dropout_ratio,
-    args.attention_type)
-model.prepare_translation(
-    stoi[JA.init_token], stoi[JA.eos_token], JA.vocab.itos, args.max_length)
-
-if args.gpu >= 0:
-    model.cuda(args.gpu)
-
-optimizer = optim.Adam(
-    model.parameters(), lr=args.learning_rate, weight_decay=args.weight_decay)
-
-max_bleu = -1
-sum_loss = 0
-max_epoch = args.epoch
-eval_step = args.evaluation_step
-previous_epoch = 0.
-
-print('start training')
-for batch in train_iter:
-    optimizer.zero_grad()
-    loss = model.compute_loss((batch.src, batch.trg))
-    loss.backward()
-    torch.nn.utils.clip_grad_norm(model.parameters(), args.gradient_clipping)
-    optimizer.step()
-    sum_loss += loss.data[0]
-    del loss, batch
-    # report loss
-    if train_iter.epoch // 1 != previous_epoch // 1:
-        print('epoch: {}'.format(train_iter.epoch))
-        print('loss: {}'.format(sum_loss))
-        sum_loss = 0
-    # evaluation
-    if train_iter.epoch // eval_step != previous_epoch // eval_step:
-        # evaluation model
-        model.eval()
-        bleu = model.evaluate(val_iter)
-        print('bleu: {}'.format(bleu))
-        if bleu > max_bleu:
-            max_bleu = bleu
-            torch.save(model.state_dict(), 'best.pt')
-        # back to training mode
-        model.train()
-    # stop training
-    if train_iter.epoch // max_epoch != previous_epoch // max_epoch:
-            break
-    previous_epoch = train_iter.epoch
-
-model.load_state_dict(torch.load('best.pt'))
-model.eval()
-print('test score: {}'.format(model.evaluate(test_iter)))
+    main(args, logger)
